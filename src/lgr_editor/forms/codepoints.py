3074435ce7043ac44322c0bed4e2612a6d3ebd62
Audric Schiltknecht <audric.schiltknecht@viagenie.ca>
2017-10-11 13:09:44 -0400

    Emphasize that set labels are optional in form

--- src/lgr_editor/forms/codepoints.py
+++ src/lgr_editor/forms/codepoints.py
@@ -91,9 +91,10 @@ class ValidateLabelForm(forms.Form):
     label = forms.CharField(label=_("Label"))
     set_labels = forms.FileField(label=_("Allocated Set labels"),
                                  required=False,
-                                 help_text=_('Labels already allocated in the LGR set, '
-                                             'that will be used to check for collisions '
-                                             'when evaluating labels on the LGR set'))
+                                 help_text=_('Optional list of labels already allocated '
+                                             'in the LGR set, that will be used to check '
+                                             'for collisions when evaluating labels using '
+                                             'the merged LGR set'))
     script = forms.ChoiceField(label=_("Script"),
                                required=False,
                                help_text=_('The script used to validate the label'))
