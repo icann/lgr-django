49937cd5986dd1c367be347d5e1119d337b309c5
Audric Schiltknecht <audric.schiltknecht@viagenie.ca>
2018-03-05 10:48:30 -0500

    Add link to retrieve HTML output as attachment
    Some browser (ie. IE/Edge) will transform relative links (anchors) to
    absolute links to the current domain name. This make the saved HTML
    document impossible to use offline as it will try to open links to the
    lgr-django server instead.

--- src/lgr_renderer/views.py
+++ src/lgr_renderer/views.py
@@ -20,3 +20,12 @@ class LGRRendererView(TemplateView):
         context = super(LGRRendererView, self).get_context_data(**kwargs)
         context.update(generate_context(self.lgr_info.lgr))
         return context
+
+    def render_to_response(self, context, **response_kwargs):
+        """
+        Render the template as an attachment if required.
+        """
+        html_response = super(LGRRendererView, self).render_to_response(context, **response_kwargs)
+        if 'save' in self.request.GET:
+            html_response['Content-Disposition'] = 'attachment; filename="{}.html"'.format(self.lgr_info.lgr)
+        return html_response
