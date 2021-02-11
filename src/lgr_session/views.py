c2f1756f933676126f5fd35d17a30096c8430fe2
Julien BERNARD <julien.bernard@viagenie.ca>
2021-02-11 11:14:46 -0500

    Prevent user from seeing ICANN report

--- src/lgr_session/views.py
+++ src/lgr_session/views.py
@@ -1,20 +1,28 @@
 # -*- coding: utf-8 -*-
 from django.contrib import messages
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.core.exceptions import SuspiciousOperation
 from django.http import Http404, FileResponse
 from django.shortcuts import redirect
 from django.utils.translation import ugettext_lazy as _
 from django.views.generic.base import View
 
 from lgr_advanced.api import LgrToolSession
+from lgr_advanced.lgr_editor.views import RE_SAFE_FILENAME
+from lgr_auth.models import LgrRole
 from lgr_idn_table_review.icann.api import LgrIcannSession
 from lgr_idn_table_review.tool.api import LgrIdnReviewSession
 
 
-class LgrSessionView(View):
+class LgrSessionView(UserPassesTestMixin, View):
 
     def dispatch(self, request, *args, **kwargs):
         self.filename = self.kwargs.get('filename')
+        if not RE_SAFE_FILENAME.match(self.filename):
+            raise SuspiciousOperation()
         self.folder = self.kwargs.get('folder', None)
+        if not RE_SAFE_FILENAME.match(self.folder):
+            raise SuspiciousOperation()
         self.next = request.GET.get('next', '/')
         storage_type = self.kwargs.get('storage')
         if storage_type == 'tool':
@@ -26,6 +34,13 @@ class LgrSessionView(View):
         else:
             raise Http404
         return super().dispatch(request, *args, **kwargs)
+
+    def test_func(self):
+        storage_type = self.kwargs.get('storage')
+        if storage_type == 'rev_icann':
+            return self.request.user.is_authenticated and self.request.user.role in [LgrRole.ICANN.value,
+                                                                                     LgrRole.ADMIN.value]
+        return True
 
 
 class DownloadFileView(LgrSessionView):
