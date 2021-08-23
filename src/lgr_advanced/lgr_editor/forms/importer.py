9338866be21a25ea6047cd11fa62a4ef15c13db1
Guillaume Blanchet <guillaume.blanchet@viagenie.ca>
2021-08-23 11:23:42 -0400

    temporary fix: we are waiting for Julien's refactoring to avoid using the session in import/create form

--- src/lgr_advanced/lgr_editor/forms/importer.py
+++ src/lgr_advanced/lgr_editor/forms/importer.py
@@ -7,29 +7,25 @@ from django.utils.translation import ugettext_lazy as _
 
 from lgr_models.models.lgr import RzLgr
 from .fields import (DEFAULT_UNICODE_VERSION,
-                     VALIDATING_REPERTOIRES,
-                     DEFAULT_VALIDATING_REPERTOIRE,
                      FILE_FIELD_ENCODING_HELP)
 
 
-class CreateLGRForm(forms.Form):
+class NewLGRForm(forms.Form):
+    #  TODO:  we need to add the other repertoires (and not just the root zone one) as well here:
+    validating_repertoire = forms.ModelChoiceField(queryset=RzLgr.objects.all(), empty_label=None, to_field_name='name')
+
+
+class CreateLGRForm(NewLGRForm):
     name = forms.CharField(label=_("Name"))
-    validating_repertoire = forms.ChoiceField(label=_("Validating repertoire"),
-                                              help_text=_('Code points will be limited to the selected repertoire'),
-                                              required=False,
-                                              choices=(('', ''),) + VALIDATING_REPERTOIRES,
-                                              initial=DEFAULT_VALIDATING_REPERTOIRE)
     unicode_version = forms.CharField(widget=forms.HiddenInput(),
                                       initial=DEFAULT_UNICODE_VERSION)
 
 
-class ImportLGRForm(forms.Form):
+class ImportLGRForm(NewLGRForm):
     file = forms.FileField(label=_("Select file(s)"), required=True,
                            help_text=f"{_('If you select more than one file, this will create a LGR set.')} "
                                      f"{FILE_FIELD_ENCODING_HELP}",
                            widget=forms.ClearableFileInput(attrs={'multiple': True}))
-    #  TODO:  we need to add the other repertoires (and not just the root zone one) as well here:
-    validating_repertoire = forms.ModelChoiceField(queryset=RzLgr.objects.all(), empty_label=None, to_field_name='name')
     set_name = forms.CharField(label=_("LGR set name"),
                                required=False,
                                # TODO should catch that to get a valid LGR name
