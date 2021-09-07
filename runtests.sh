4339386dd540ca95b8252c79f3119dedac74f7d5
Julien BERNARD <julien.bernard@viagenie.ca>
2021-09-07 11:00:05 -0400

    Remove unused script

--- runtests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Small helper to execute all tests.
-# TODO: replace by something more pythonic...
-
-export PYTHONPATH=$(dirname $0)
-
-echo Run doctests
-python2 -m doctest src/lgr_editor/utils.py
