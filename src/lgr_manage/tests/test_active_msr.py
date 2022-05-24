74979cb15621529221722b02dcdbcae577f44eea
Michel Bernier <mbernier@cofomo.com>
2022-05-24 12:17:00 -0400

    add constants to tests.

--- src/lgr_manage/tests/test_active_msr.py
+++ src/lgr_manage/tests/test_active_msr.py
@@ -1,6 +1,8 @@
+from http import HTTPStatus
+
 from lgr_models.tests.lgr_webclient_test_base import LgrWebClientTestBase
-from lgr_manage.forms import MSRIsActiveForm
 from lgr_models.models.lgr import MSR
+
 
 class MSRActiveTestCase(LgrWebClientTestBase):
     def test_access_active_when_logged_in(self):
@@ -9,22 +11,21 @@ class MSRActiveTestCase(LgrWebClientTestBase):
         response = self.client.get('/m/msr')
         self.assertContains(response,
                             '<form class="form-horizontal" id="active-choice-form" url-data="/m/msr/isactive">',
-                            status_code=200)
+                            status_code=HTTPStatus.OK)
 
     def test_access_active_when_not_logged_in(self):
         response = self.client.get('/m/msr')
-        self.assertContains(response, status_code=403)
+        self.assertEquals(response.status_code, HTTPStatus.FOUND)
+        self.assertEquals(response.url, '/auth/login?next=/m/msr')
 
     def test_update_active_when_logged_in(self):
         self.login()
-        form = MSRIsActiveForm()
-        form.data = {'active': ['2']}
-        response = self.client.post('/m/msr/isactive', form)
-        self.assertContains(response, status_code=200)
-        self.assertContains(MSR.filter(active=True).first().pk, 2)
+        self.client.post('/m/msr/isactive', data={'active': 2})
+        response = self.client.post('/m/msr/isactive', data={'active': 4})
+        self.assertContains(response, '"old_active": 2', status_code=HTTPStatus.OK)
+        self.assertEquals(MSR.objects.filter(active=True).first().pk, 4)
 
     def test_update_active_when_not_logged_in(self):
-        form = MSRIsActiveForm()
-        form.data = {'active': ['2']}
-        response = self.client.post('/m/msr/isactive', form)
-        self.assertContains(response, status_code=403)
+        response = self.client.post('/m/msr/isactive', data={'active': 2})
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertEquals(response.url, '/auth/login?next=/m/msr/isactive')
--- src/lgr_manage/tests/test_active_rz_lgr.py
+++ src/lgr_manage/tests/test_active_rz_lgr.py
@@ -1,5 +1,6 @@
+from http import HTTPStatus
+
 from lgr_models.tests.lgr_webclient_test_base import LgrWebClientTestBase
-from lgr_manage.forms import RzLgrIsActiveForm
 from lgr_models.models.lgr import RzLgr
 
 
@@ -10,21 +11,21 @@ class RzLgrActiveTestCase(LgrWebClientTestBase):
         response = self.client.get('/m/rz-lgr')
         self.assertContains(response,
                             '<form class="form-horizontal" id="active-choice-form" url-data="/m/rz-lgr/isactive">',
-                            status_code=200)
+                            status_code=HTTPStatus.OK)
 
     def test_access_active_when_not_logged_in(self):
         response = self.client.get('/m/rz-lgr')
-        self.assertEquals(response.status_code, 302)
+        self.assertEquals(response.status_code, HTTPStatus.FOUND)
         self.assertEquals(response.url, '/auth/login?next=/m/rz-lgr')
 
     def test_update_active_when_logged_in(self):
         self.login()
         self.client.post('/m/rz-lgr/isactive', data={'active': 2})
         response = self.client.post('/m/rz-lgr/isactive', data={'active': 4})
-        self.assertContains(response, '"old_active": 2', status_code=200)
+        self.assertContains(response, '"old_active": 2', status_code=HTTPStatus.OK)
         self.assertEquals(RzLgr.objects.filter(active=True).first().pk, 4)
 
     def test_update_active_when_not_logged_in(self):
         response = self.client.post('/m/rz-lgr/isactive', data={'active': 2})
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
         self.assertEquals(response.url, '/auth/login?next=/m/rz-lgr/isactive')
