9e8408621c1445a4b2b15ce2287be5efb5c4688f
Michel Bernier <mbernier@cofomo.com>
2022-05-24 09:59:59 -0400

    Add Tests for active rzlgr and msr.

--- src/lgr_manage/tests/test_active_rz_lgr.py
+++ src/lgr_manage/tests/test_active_rz_lgr.py
@@ -1,14 +1,14 @@
 from lgr_models.tests.lgr_webclient_test_base import LgrWebClientTestBase
-from lgr_manage.forms import MSRIsActiveForm
-from lgr_models.models.lgr import MSR
+from lgr_manage.forms import RzLgrIsActiveForm
+from lgr_models.models.lgr import RzLgr
 
-class MSRActiveTestCase(LgrWebClientTestBase):
+class RzLgrActiveTestCase(LgrWebClientTestBase):
     def test_access_active_when_logged_in(self):
         self.login()
 
-        response = self.client.get('/m/msr')
+        response = self.client.get('/m/rz-lgr/isactive')
         self.assertContains(response,
-                            '<form class="form-horizontal" id="active-choice-form" url-data="/m/msr/isactive">',
+                            '<form class="form-horizontal" id="active-choice-form" url-data="/m/rz-lgr/isactive">',
                             status_code=200)
 
     def test_access_active_when_not_logged_in(self):
@@ -17,14 +17,14 @@ class MSRActiveTestCase(LgrWebClientTestBase):
 
     def test_update_active_when_logged_in(self):
         self.login()
-        form = MSRIsActiveForm()
+        form = RzLgrIsActiveForm()
         form.data = {'active': ['2']}
-        response = self.client.post('/m/msr/isactive', form)
+        response = self.client.post('/m/rz-lgr/isactive', form)
         self.assertContains(response, status_code=200)
-        self.assertContains(MSR.filter(active=True).first().pk, 2)
+        self.assertContains(RzLgr.filter(active=True).first().pk, 2)
 
     def test_update_active_when_not_logged_in(self):
-        form = MSRIsActiveForm()
+        form = RzLgrIsActiveForm()
         form.data = {'active': ['2']}
-        response = self.client.post('/m/msr/isactive', form)
+        response = self.client.post('/m/rz-lgr/isactive', form)
         self.assertContains(response, status_code=403)
