ac389b9559d913114e5a958660847440b7c0a18e
Julien BERNARD <jbernard@cofomo.com>
2022-11-16 15:10:36 -0500

    Allow running migrations when settings table is not created

--- src/lgr_web/config.py
+++ src/lgr_web/config.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from lgr_models.models.settings import LGRSettings
 
-lgr_settings: LGRSettings = LGRSettings.objects.get(pk=1)
+lgr_settings = None
+try:
+    # LGRSettings may not exist when launching migrations so we have to encapsulate in a try..except
+    from lgr_models.models.settings import LGRSettings
+    lgr_settings: LGRSettings = LGRSettings.objects.get(pk=1)
+except:
+    pass
