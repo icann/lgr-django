cdb7557262c14afe7206f9486810f6450f9be8fd
Julien BERNARD <jbernard@cofomo.com>
2023-06-14 15:38:37 -0400

    Add test for autocomplete list with core requirements

--- src/lgr_utils/tests/test_RefLgrAutocomplete.py
+++ src/lgr_utils/tests/test_RefLgrAutocomplete.py
@@ -1,6 +1,6 @@
 from unittest.mock import patch
 
-from lgr_utils.views import RefLgrAutocomplete
+from lgr_utils.views import RefLgrAutocomplete, RefLgrAutocompleteWithCore, RFC_CORE_REQUIREMENT
 from django.test import SimpleTestCase
 from lgr_utils.tests.ModelMocks import RefLgrMemberMock, RzLgrMock, RefLgrMock, MockListExpected
 
@@ -18,3 +18,12 @@ class test_RefLgrAutocomplete_building(SimpleTestCase):
     def test_normalRefLgrAutocomplete(self):
         response = RefLgrAutocomplete.get_list()
         self.assertEqual(response, MockListExpected)
+
+
+    @patch("lgr_models.models.lgr.RefLgr.objects", RefLgrMock())
+    @patch("lgr_models.models.lgr.RefLgrMember.objects", RefLgrMemberMock())
+    @patch("lgr_models.models.lgr.RzLgr.objects", RzLgrMock())
+    def test_normalRefLgrAutocompleteWithCore(self):
+        response = RefLgrAutocompleteWithCore.get_list()
+        self.assertEqual(response, MockListExpected + [(('RFC Core Requirements', 'RFC Core Requirements'),
+                         ((RFC_CORE_REQUIREMENT, 'RFC Core Requirements'),))])
