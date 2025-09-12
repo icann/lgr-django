44ee8af6d223a46800333ad34bfd1b5c99c841d0
Alexandre Taillon-Desrochers <atdesrochers@cofomo.com>
2025-09-12 16:22:32 -0400

    Fix: Allow usage of a test settings file instead of local

--- src/lgr_web/settings/__init__.py
+++ src/lgr_web/settings/__init__.py
@@ -1,4 +1,9 @@
-from .local import *
+#TODO: Have a better way to handle the import of settings when using
+# something else than local
+try:
+    from .test import *
+except ImportError:
+    from .local import *
 
 if SECRET_KEY == 'PLEASE GENERATE ONE':
     print("default SECRET_KEY must be replaced")
