4910b3920abea15db09d1ea845cc918d9e417b7b
Alexandre Taillon-Desrochers <atdesrochers@cofomo.com>
2025-09-24 14:18:59 -0400

    Fix: Move register_converter() calls into one place and remove registration duplication

--- src/lgr_advanced/urls.py
+++ src/lgr_advanced/urls.py
@@ -7,14 +7,11 @@ The `urlpatterns` list routes URLs to views. For more information, please see:
 from django.conf.urls import include
 from django.urls import path
 
-import lgr_advanced.lgr_editor.urls
-import lgr_advanced.lgr_tools.urls
-import lgr_advanced.lgr_validator.urls
 from lgr_advanced.views import AdvancedModeView
 
 urlpatterns = [
-    path('editor/', include(lgr_advanced.lgr_editor.urls.urlpatterns)),
-    path('validator/', include(lgr_advanced.lgr_validator.urls.urlpatterns)),
-    path('tools/', include(lgr_advanced.lgr_tools.urls.urlpatterns)),
+    path('editor/', include('lgr_advanced.lgr_editor.urls')),
+    path('validator/', include('lgr_advanced.lgr_validator.urls')),
+    path('tools/', include('lgr_advanced.lgr_tools.urls')),
     path('', AdvancedModeView.as_view(), name='lgr_advanced_mode'),
 ]
