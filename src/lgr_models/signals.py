b3cd9cd55158c79f066a4be203e9e05df83bf192
Alexandre Taillon-Desrochers <atdesrochers@cofomo.com>
2025-09-26 15:23:24 -0400

    Fix: Delete parent folder when deleting a model with file and folder is empty

--- src/lgr_models/signals.py
+++ src/lgr_models/signals.py
@@ -1,15 +1,19 @@
-#! /bin/env python
-# -*- coding: utf-8 -*-
 """
 signal.py - Signal used to handle folder deletion when a file is removed with django-cleanup
 """
-
+import logging
 import os
+from pathlib import Path
+
+logger = logging.getLogger(__name__)
 
 
 def delete_parent_folder(sender, **kwargs):
     try:
-        os.rmdir(os.path.dirname(kwargs['file']))
-    except:
-        # if dir is not empty, do nothing
+        os.rmdir(os.path.dirname(Path(kwargs['file'].storage.location) / kwargs['file_name']))
+    except OSError:
+        # The directory is not empty
+        pass
+    except (Exception, BaseException) as e:
+        logger.warning(f'Could not delete folder: {e}')
         pass
